0c707f7e5a3dd5f5b4a0076fd4abc326f48d1943
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-12-09 20:53:28 +0530

    Create Insert-Interval.cpp

---
+++ Insert-Interval.cpp
@@ -0,0 +1,90 @@
+######################################################################################################  Question  ####################################################################################################################################
+
+Geek has an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith event and intervals is sorted in ascending order by starti. He wants to add a new interval newInterval= [newStart, newEnd] where newStart and newEnd represent the start and end of this interval.
+
+Help Geek to insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Examples:
+
+Input: intervals = [[1,3], [4,5], [6,7], [8,10]], newInterval = [5,6]
+Output: [[1,3], [4,7], [8,10]]
+Explanation: The newInterval [5,6] overlaps with [4,5] and [6,7].
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,9]
+Output: [[1,2], [3,10], [12,16]]
+Explanation: The new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+Constraints:
+1 ≤ intervals.size() ≤  105
+0 ≤ start[i], end[i] ≤ 109
+
+######################################################################################################  Solution  ####################################################################################################################################
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
+                                       vector<int> &newInterval) {
+            vector<vector<int>> ans;
+        bool merged=false;
+        int start=newInterval[0],end=newInterval[1];
+        
+        for(auto x:intervals){
+            if(x[1]<newInterval[0]){
+                ans.push_back(x);
+            }
+            else if(x[0]>newInterval[1]){
+                if(!merged){
+                    ans.push_back({start,end});
+                    merged=true;
+                }
+                ans.push_back(x);
+            }
+            else{
+                start=min(start,x[0]);
+                end=max(end,x[1]);
+            }
+        }
+        
+        if(!merged)
+        ans.push_back({start,end});
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> intervals(N, vector<int>(2));
+        for (int i = 0; i < N; i++) {
+            cin >> intervals[i][0] >> intervals[i][1];
+        }
+        vector<int> newInterval(2);
+        cin >> newInterval[0] >> newInterval[1];
+
+        Solution obj;
+        vector<vector<int>> ans = obj.insertInterval(intervals, newInterval);
+        cout << "[";
+        for (int i = 0; i < ans.size(); i++) {
+            cout << "[" << ans[i][0] << ',' << ans[i][1] << ']';
+            if (i != (ans.size() - 1))
+                cout << ",";
+        }
+        cout << "]" << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
